c452338f5333ea620ed6f6f789cc3b80a653b2d5
Matt Ballard <mattjballard81@gmail.com>
2024-06-24 15:27:16 +0800

    set up additional form validation for user registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:9', 'confirmed']
         ]);
+
+        // $incomingFields['password'] = bcrpyt($incomingFields['password']);
+
         User::create($incomingFields);
         return 'User registered!';
     }
